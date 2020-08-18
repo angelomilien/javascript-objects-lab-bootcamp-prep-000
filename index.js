b8ff9fd966ed69654544f45af1fe39f48766902c
Angelo milien <angelomilienjoseph@gmail.com>
2020-08-18 22:57:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
  
  
  function destructivelyDeleteFromObjectByKey(object, key) {
-    delete object.key
+    return delete recipes.prop
    
  }
  
